57cb536f603d8661f48f50db90defdae8e4cd4ad
Muniz <muniz@webtagger.com.br>
2023-03-23 21:53:25 -0300

    delete photo and update create root to react 18+

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(
+const root = document.getElementById("root");
+
+const aplication = ReactDOMClient.createRoot(root);
+
+aplication.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
