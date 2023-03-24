8d4ab84e93e39cc4fe4f5b462d73bd0bef2ca6d2
Muniz <muniz@webtagger.com.br>
2023-03-23 21:06:02 -0300

    comment post

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -16,7 +16,7 @@ const FeedModal = ({ photo, setModalPhoto }) => {
   }, [photo, request]);
 
   function handleOutsideClick(event) {
-    event.target === event.currentTarget && setModalPhoto(null);
+    if (event.target === event.currentTarget) setModalPhoto(null);
   }
 
   return (
